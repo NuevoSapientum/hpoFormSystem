92860fc31d33f42d983d2b865eed3eef5069e48c
Erwin Mark <bananasapientum@gmail.com>
2015-09-09 17:01:02 +0800

    Edit Profile(v.0.1)

--- resources/views/auth/editProfile.blade.php
+++ resources/views/auth/editProfile.blade.php
@@ -3,3 +3,10 @@
 @section('head')
 	<h1>Edit Profile</h1>
 @endsection
+
+@section('content')
+	<form method="POST" action="#">
+		<input type="text" class="form-control" value="{{Auth::user()->emp_name}}"/>
+		<input type="text" class="form-control" value="{{Auth::user()->emp_position}}"/>
+	</form>
+@endsection
