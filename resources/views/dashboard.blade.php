3a1e0c50ea1357968049e0993bf4f149d9637b79
NuevoSapientum <bananasapientum@gmail.com>
2015-10-28 23:59:49 -0800

    Updates(v.0.35)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,12 +13,32 @@
         
         <div class="well well-sm">
         <h1>
-          Home Page
+          HP Outsourcing E-request System
         </h1>  
       </div>
       @endsection
 
       @section('content')
-
+          <p style="font-size: 15px" class="text-justify">
+            It is an E-request form system that will provide online form submission and validation that the 
+            employee will use in filing there exit pass, request for leave of absence, overtime authorization and 
+            change schedule. 
+          </p>
+          <br/>
+          <label>Types of Forms:</label>
+          <hr/>
+          <pre>
+              <strong>Exit pass</strong>
+          -form template if you want to go outside the company for a period of time during working hours.
+              <br/>
+              <strong> Request for Leave of Absence</strong>
+          -form template if you want to apply for a leave of absence (eg. Sick Leave, Vacation Leave, Maternal Leave, Paternal Leave )
+              <br/>
+              <strong>Overtime Authorization</strong>
+          -form template if you want to apply for an overtime.
+              <br/>
+              <strong>Change schedule</strong>
+          -form template if you want to change your schedule.
+          </pre>
       @endsection
 
